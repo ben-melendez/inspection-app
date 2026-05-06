3094828b99d4e659dceb6586d5a76707e1acbba4
Benjamin Melendez <benjaminmelendez@Benjamins-MacBook-Air.local>
2026-05-06 16:36:31 -0700

    fix typescript env error

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@
 import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 ``
 const supabase = createClient(supabaseUrl, supabaseKey);
 
